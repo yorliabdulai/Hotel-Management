27e5295d6a8299afe0c4265ab8398c94f3606a0f
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-05 19:54:27 +0000

    created the auth component which is rendered as the signup/login page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,28 @@
+import {AiFillGithub} from 'react-icons/ai'
+import {FcGoogle} from 'react-icons/fc'
+
+const Auth = () => {
+  const inputStyles = 'border border-gray-300 sm:text-sm text-black rounded:lg block w-full p-2.5 focus:outline-none'
+    return(
+        <section className="container mx-auto">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
+              <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
+                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">Create an account</h1>
+                <p>OR</p>
+                <span className="inline-flex items-center">
+                  <AiFillGithub className="mr-3 text-4xl cursor-pointer text-black dark:text-white"/> | 
+                  <FcGoogle className="ml-3 text-4xl cursor-pointer"/>
+                </span>
+              </div>
+              <form className="space-y-4 md:space-y-6">
+              <input type='text' name='name' id='name' placeholder='Abdulai Iddrisu' required className={inputStyles}/>
+                <input type='email' name='email' id='email' placeholder='email@company.com' required className={inputStyles}/>
+                <input type='password' name='password' id='password' placeholder='password' required minLength={6} className={inputStyles}/>
+                <button type='submit' className='w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign Up</button>
+              </form>
+              <button className='text-blue-700 underline'>login</button>
+            </div>
+        </section>
+    )
+}
+export default Auth
