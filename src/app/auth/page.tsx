7bcd6db85eac16c1db3405b889b976a9e1cfa718
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-06 12:37:57 +0000

    Added form data handling logic to be able to retrieve and submit form data which is currently being logged to the console

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import {AiFillGithub} from 'react-icons/ai'
 import {FcGoogle} from 'react-icons/fc'
-
+const defaultFormData ={
+  email: '',
+  name: '',
+  password: '',
+}
 const Auth = () => {
+  const [formData, setFormData] = useState(defaultFormData)
   const inputStyles = 'border border-gray-300 sm:text-sm text-black rounded:lg block w-full p-2.5 focus:outline-none'
-    return(
+   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) =>{
+    const {name, value} = event.target;
+    setFormData({...formData, [name]: value});
+   };
+   const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
+    event.preventDefault()
+    try {
+      console.log(formData)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setFormData(defaultFormData);
+    }
+   };
+  return(
         <section className="container mx-auto">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
               <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
@@ -14,10 +35,10 @@ const Auth = () => {
                   <FcGoogle className="ml-3 text-4xl cursor-pointer"/>
                 </span>
               </div>
-              <form className="space-y-4 md:space-y-6">
-              <input type='text' name='name' id='name' placeholder='Abdulai Iddrisu' required className={inputStyles}/>
-                <input type='email' name='email' id='email' placeholder='email@company.com' required className={inputStyles}/>
-                <input type='password' name='password' id='password' placeholder='password' required minLength={6} className={inputStyles}/>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <input type='text' name='name' value={formData.name} onChange={handleInputChange} placeholder='Abdulai Iddrisu' required className={inputStyles}/>
+                <input type='email' name='email' value={formData.email} onChange={handleInputChange} placeholder='email@company.com' required className={inputStyles}/>
+                <input type='password' name='password' value={formData.password} onChange={handleInputChange} placeholder='password' required minLength={6} className={inputStyles}/>
                 <button type='submit' className='w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign Up</button>
               </form>
               <button className='text-blue-700 underline'>login</button>
