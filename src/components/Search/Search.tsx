2ffedecc13e69c6a7d36560671c75a7b67669882
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-12 11:20:51 +0000

    Add room type filter and search query to search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,23 @@
-const Search = () => {
+'use client'
+import { useRouter } from "next/router";
+import { ChangeEvent, FC } from "react";
+type Props = {
+    roomTypeFilter: string;
+    searchQuery: string;
+    setRoomTypeFilter: (value: string) => void;
+    setSearchQuery: (value: string) => void;
+}
+const Search: FC<Props> = ({roomTypeFilter, searchQuery, setRoomTypeFilter, setSearchQuery}) => {
+    const router = useRouter()
+    const handleRoomTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setRoomTypeFilter(event.target.value)
+    };
+    const handleSearchQueryChange = (event: ChangeEvent<HTMLInputElement>) =>{
+        setSearchQuery(event.target.value);
+    }
+    const handleFilterClick = () => {
+      router.push(`/rooms?roomType=${roomTypeFilter} &searchQuery=${searchQuery}`)
+    }
  return (
     <section className="bg-tertiary-light px-4 py-6 rounded-lg">
         <div className="container mx-auto flex items-center gap-4 justify-between flex-wrap">
@@ -8,8 +27,8 @@ const Search = () => {
                 </label>
                 <div className="relative">
                     <select 
-                    //value={roomTypeFilter}
-                    //onChange={handleRoomTypeChange} 
+                    value={roomTypeFilter}
+                    onChange={handleRoomTypeChange} 
                     className=" w-full px-4 py-2 capitalize rounded leading-tight dark:bg-black focus:outline-none">
                       <option value='All'>All</option>
                       <option value='Basic'>Basic</option>
@@ -25,14 +44,14 @@ const Search = () => {
                   id="search" 
                   placeholder="Search..."
                   className="w-full px-4 py-3 rounded leading-tight dark:bg-black focus:outline-none placeholder:text-black dark:placeholder:text-white"
-                  //value={searchQuery}
-                 // onChange={handleSearchQueryChange}
+                  value={searchQuery}
+                  onChange={handleSearchQueryChange}
                   />
             </div>
             <button 
             className="btn-primary"
             type="button"
-            //onClick={handleFilterClick}
+            onClick={handleFilterClick}
             >Search</button>
         </div>
     </section>
