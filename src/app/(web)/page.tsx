37d99cb42dac70b714b372218926db7d11505702
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-13 08:55:50 +0000

    Created and Add Gallery component to the home page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,10 +1,11 @@
 import HeroSection from "@/components/HeroSection/HeroSection"
 import PageSearch from "@/components/PageSearch/PageSearch"
-
+import Gallery from "@/components/Gallery/Gallery"
 const Home = () => {
   return (
     <><HeroSection />
     <PageSearch />
+    <Gallery />
     </>
   )
 }
