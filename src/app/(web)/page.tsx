e609c5e229bc3f8756d7642a2288f046a96ee871
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-13 10:47:51 +0000

    Created and  Add NewsLetter component to the home page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection/HeroSection"
 import PageSearch from "@/components/PageSearch/PageSearch"
 import Gallery from "@/components/Gallery/Gallery"
+import NewsLetter from "@/components/NewsLetter/NewsLetter"
 const Home = () => {
   return (
     <><HeroSection />
     <PageSearch />
     <Gallery />
+    <NewsLetter />
     </>
   )
 }
