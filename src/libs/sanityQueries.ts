aa5815cc935b42db8793a042ee26572bfe2788be
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-14 09:04:16 +0000

    Update featured room query to only return the first featured room.

--- src/libs/sanityQueries.ts
+++ src/libs/sanityQueries.ts
@@ -1,3 +1,13 @@
 import { groq } from "next-sanity";
 
-export const getFeaturedRoomQuery = groq`*[_type == "hotelRoom"]`
+export const getFeaturedRoomQuery = groq`*[_type == "hotelRoom" && isFeatured == true][0]{
+    _id,
+    description,
+    discount,
+    images,
+    isFeatured,
+    name,
+    price,
+    slug,
+    
+}`
