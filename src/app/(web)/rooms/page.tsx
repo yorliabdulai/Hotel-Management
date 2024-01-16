e43ceebc19a35c20b66f31856722506afa957585
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-16 19:23:17 +0000

    created rooms page to render rooms and utilizes 'useSearchParams' from Next.js navigation for managing search parameters, allowing users to filter rooms based on both search queries and room types.

---
+++ src/app/(web)/rooms/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const Rooms = () =>{
+    const [roomTypeFilter, setRoomTypeFiter] = useState("");
+    const [searchQuery, setSeachQuery] = useState("");
+    const searchParams = useSearchParams();
+    useEffect(()=>{
+      const searchQuery = searchParams.get("searchQuery")
+      const roomType = searchParams.get("roomType")
+    },[])
+    return (
+        <div className="rooms"></div>
+    )
+}
+export default Rooms;
