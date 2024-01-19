4230bd7dee114cb8a55ccb2cf940c1fea52adf89
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-19 19:09:21 +0000

    Add SWR for fetching room data

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -1,9 +1,18 @@
 "use client"
 
+import { getRoom } from "@/libs/apis";
+import useSWR from "swr";
+import LoadingSpinner from "../../loading";
+
 const RoomDetails = (props: {params: {slug: string}}) => {
     const {params: {slug},
 } = props;
-
+const fetchRoom = async () => getRoom(slug)
+const {data: room, error, isLoading} = useSWR("/api/room", fetchRoom);
+    if (error) throw new Error("Cannot fetch data");
+    if (typeof room === "undefined" && !isLoading) throw new Error("Cannot fetch data");
+    if (!room) return <LoadingSpinner />
+    
     return (
         <div>
             <h1>Room Details</h1>
