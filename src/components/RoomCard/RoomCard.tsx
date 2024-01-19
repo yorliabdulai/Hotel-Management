a72db1eca073134f57c9d8e704ca7c788254e904
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-19 13:59:51 +0000

    Add room cover image, description, and booking status to the room card component.

--- src/components/RoomCard/RoomCard.tsx
+++ src/components/RoomCard/RoomCard.tsx
@@ -1,5 +1,8 @@
-import { Room } from "@/app/models/room";
 import { FC } from "react"
+import Image from "next/image"
+import { Room } from "@/app/models/room";
+import Link from "next/link";
+
 type Props = {
     room: Room;
 }
@@ -7,13 +10,35 @@ const RoomCard: FC<Props> = props =>{
     const {
         room: {
         name,
-        type,
+        coverImage,
         price,
+        type,
+        description,
+        slug,
+        isBooked
     
     }} = props;
   return (
-    <div>
-      
+    <div className="rounded-xl w-72 mb-10 mx-auto md:mx-0 overflow-hidden text-black">
+      <div className="h-60 overflow-hidden">
+        <Image src={coverImage.url}
+         alt={name}
+         width={250}
+         height={250}
+         className="img scale-animation"
+         />
+      </div>
+      <div className="p-4 bg-white">
+        <div className="flex justify-between text-2xl font-semibold">
+            <p>{name}</p>
+            <p>${price}</p>
+        </div>
+        <p className="pt-2 text-xs">{type} Room</p>
+        <p className="pt-3 pb-6">{description}</p>
+        <Link href={`/rooms/${slug.current}`} className="bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:translate-y-2 hover:shadow-lg transition-all duration-500" >
+            {isBooked ? "BOOKED" : "BOOK NOW"}
+        </Link>
+      </div>
     </div>
   )
 }
