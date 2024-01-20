16ce99ee26db455892eadf4afb6ac80c7e9e76e4
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-20 08:40:03 +0000

    Add Next.js Image component and useState hook to HotelPhotoGallery component

--- src/components/HotelPhotoGallery/HotelPhotoGallery.tsx
+++ src/components/HotelPhotoGallery/HotelPhotoGallery.tsx
@@ -1,11 +1,31 @@
-import { Image } from "@/app/models/room";
-import { FC } from "react";
+"use client"
+import { Image as ImageType } from "@/app/models/room";
+import Image from "next/image";
+import { FC, useState } from "react";
 
 
-const HotelPhotoGallery: FC<{photos: Image[]}> = () => {
+const HotelPhotoGallery: FC<{photos: ImageType[]}> = ({photos}) => {
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const openModal = (index: number) =>{
+        setCurrentPhotoIndex(index);
+        setShowModal(true);
+    }
     return (
         <div className="container mx-auto">
-            {/* Your photo gallery code goes here */}
+            <div className="grid md:grid-cols-2 relative gap-5 px-3">
+                <div className="h-[540px] relative rounded-2xl overflow-hidden">
+                    <div className="hidden md:flex justify-center items-center w-full h-full">
+                        <Image src={photos[0].url} 
+                        alt={`Room Photo ${currentPhotoIndex + 1}`}
+                         className="img scale-animation cursor-pointer" 
+                         width={150} 
+                         height={150}
+                         onClick={openModal.bind(this, 0)}
+                         />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
