5b85d7e7e0c55839604f8b156db30cca38024a25
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-22 15:22:41 +0000

    Update HotelPhotoGallery component to display 4 photos instead of 3.

--- src/components/HotelPhotoGallery/HotelPhotoGallery.tsx
+++ src/components/HotelPhotoGallery/HotelPhotoGallery.tsx
@@ -18,7 +18,7 @@ const HotelPhotoGallery: FC<{photos: ImageType[]}> = ({photos}) => {
     const handleNext  = () =>{
         setCurrentPhotoIndex(prevIndex => prevIndex === photos.length - 1 ? 0: prevIndex + 1);
     }
-    const maximumVisiblePhotos = 3;
+    const maximumVisiblePhotos = 4;
     const totalPhotos = photos.length;
     const displayPhotos = photos.slice(1, maximumVisiblePhotos-1);
     const remainingPhotosCount = totalPhotos - maximumVisiblePhotos
@@ -54,7 +54,34 @@ const HotelPhotoGallery: FC<{photos: ImageType[]}> = ({photos}) => {
                     {currentPhotoIndex + 1} / {photos.length}
                   </span>
                 </div>
+                <div className="hidden md:grid grid-cols-2 h-full gap-5">
+                    {displayPhotos.map((photo, index) => (
+                        <div key={index} className="cursor-pointer rounded-2xl h-64 overflow-hidden">
+                            <Image src={photo.url} 
+                            alt={`Room Photo ${index + 2}`}
+                             className="img scale-animation" 
+                             width={150} 
+                             height={150}
+                             onClick={openModal.bind(this, index + 1)}
+                             />
+                        </div>
+                    ))}
+                    {remainingPhotosCount > 0 && (
+                        <div className="cursor-pointer relative h-64 rounded-2xl overflow-hidden" onClick={openModal.bind(this, maximumVisiblePhotos)}>
+                            <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                                <p className="text-white text-2xl font-semibold">{remainingPhotosCount}+</p>
+                            </div>
+                            <Image src={photos[maximumVisiblePhotos - 1].url} 
+                            alt={`Room Photo ${maximumVisiblePhotos}`}
+                             className="img " 
+                             width={150} 
+                             height={150}
+                             //onClick={openModal.bind(this, photos.length - 1)}
+                             />
+                        </div>
+                    )}
             </div>
+        </div>
         </div>
     );
 };
