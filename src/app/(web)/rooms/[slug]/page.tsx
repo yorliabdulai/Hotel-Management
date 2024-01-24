bb89efb8af94c07e3c8b8f013147b77ba45ceeb8
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-24 10:28:44 +0000

    Added room amenities section to the room page.

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -26,6 +26,13 @@ const {data: room, error, isLoading} = useSWR("/api/room", fetchRoom);
                             <h2 className="font-bold text-left text-lg md:text-2xl">
                                 {room.name} ({room.dimension})
                             </h2>
+                            <div className="flex my-11">
+                               {room.offeredAmenities.map(ammenity => 
+                               <div key={ammenity._key} className="md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center">
+                                <i className={`fa-solid ${ammenity.icon} md:text-2xl`}></i>
+                                <p className="text-xs md:text-base pt-3">{ammenity.amenity}</p>
+                               </div>)}
+                            </div>
                         </div>
                   </div>  
                   <div className="md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto">
