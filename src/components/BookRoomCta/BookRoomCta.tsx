4243fd76e36e9ccb24c74ad7cd233cb3830ce7a2
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-28 16:41:49 +0000

    Add specialNote to BookRoomCta component

--- src/components/BookRoomCta/BookRoomCta.tsx
+++ src/components/BookRoomCta/BookRoomCta.tsx
@@ -4,11 +4,12 @@ import { FC } from "react";
 
 type Props = {
     price: number,
-    discount: number
+    discount: number,
+    specialNote: string
 }
 
 const BookRoomCta: FC<Props> = props => {
-    const {price, discount} = props;
+    const {price, discount, specialNote} = props;
     const discountPrice = price - (price / 100)* discount;
     return (
         <div className="px-7 py-6">
@@ -19,6 +20,7 @@ const BookRoomCta: FC<Props> = props => {
                 {discount? <span className="font-bold text-xl">{" "}| discount {discount}%. Now <span className="text-tertiary-dark">$ {discountPrice}</span> </span>: ""}
             </h3>
             <div className="w-full border-b-2 border-b-secondaary my-2 "/>
+            <h4 className="my-8">{specialNote}</h4>
         </div>
     );
 };
