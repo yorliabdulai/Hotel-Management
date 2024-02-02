390417725c5a07ab1d5105b4206b984f5f31cf5c
yorliabdulai <yorliabdulai1@gmail.com>
2024-02-02 10:31:57 +0000

    Add axios dependency and make a POST request to the Sanity API to create a new booking.

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,6 +1,7 @@
 import { CreateBookingDto, Room } from "@/app/models/room";
 import sanityClient from "./sanity";
 import * as queries from "./sanityQueries"
+import axios from "axios";
 
 export async function getFeaturedRoom(){
   const result = await sanityClient.fetch<Room>(queries.getFeaturedRoomQuery,{},{cache:'no-cache'})
@@ -29,5 +30,10 @@ export async function getRoom(slug:string) {
    const mutation = {
       mutations: [{create: {_type: "booking", user: {_type: "reference", _ref: user}, hotelRoom: {_type: "reference", _ref: hotelRoom}, checkinDate, checkoutDate, numberOfDays, adults, children, totalPrice, discount}}]
    }
+   const { data } = await axios.post(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`, 
+   mutation,
+    {headers: {Authorization: `Bearer ${process.env.NEXT_PUBLIC_SANITY_API_TOKEN}`}}
+    );
+    return data;
  } 
  
