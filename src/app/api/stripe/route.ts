5d58d99dfed900f8b6721ad63f3ed2f20aa6b0b3
yorliabdulai <yorliabdulai1@gmail.com>
2024-02-04 12:07:57 +0000

    Create new POST request handler for hotel room reservation with type definitions

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,18 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2023-10-16",
+});
+
+type RequestData = {
+    checkinDate: string;
+    checkoutDate: string;
+    adults: number;
+    children: number;
+    numberOfDays: number;
+    hotelRoomSlug: string;
+}
+ export async function POST(req: Request, res: Response) {
+    const {} = await req.json;
+   
+ }
