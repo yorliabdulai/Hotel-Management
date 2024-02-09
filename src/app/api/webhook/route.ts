13b7b5ee86a0da84cd08d440a8ce4584fdda3a42
yorliabdulai <yorliabdulai1@gmail.com>
2024-02-09 15:56:24 +0000

    Add Stripe webhook route and event handling.

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -4,3 +4,14 @@ const checkout_session_completed = 'checkout.session.completed';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2023-10-16",
 });
+export async function POST(req: Request, res: Response) {
+    const reqBody = await req.text();
+    const sig = req.headers.get("stripe-signature");
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+    let event: Stripe.Event;
+    try {
+        
+    } catch (error) {
+        
+    }
+}
